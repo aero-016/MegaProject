7592d3fc5341c51785e8d9c66a8dbd5d72dca8ec
vishant <vishantkumar.vaghani@abjayon.com>
2024-06-01 19:34:32 +0530

    Added logo in Assets and changed Logo component

--- blogApp/src/components/Logo.jsx
+++ blogApp/src/components/Logo.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
+import LogoImage from "../assets/logo.svg"
 
 function Logo({width='100px'}) {
   return (
-      <img className='h-12' src="..\public\logo.svg"/>
+      <img className='h-12' src={LogoImage}/>
   )
 }
 
